4536e1f5dbaaaf838c6ca469fd405713e94c468b
VIVEK SINGH <154371350+vivekvro@users.noreply.github.com>
2024-06-27 20:44:12 +0530

    Add files via upload

---
+++ MCS011_DEC2023QP.c
@@ -0,0 +1,68 @@
+//programs are stored in comments 
+
+
+#include<stdio.h>
+// int square(int x){ //Qno 1 (d) part 1
+//     return x*x ;
+// }
+int main(){
+    // int x; //Qno 1 (d) part 2
+    // printf("Enter a integer : ");
+    // scanf("%d",&x);
+    // int sqr = square(x);
+    // printf("Square of %d is %d",x,sqr);
+    
+    /********************************************************************************* */
+    // Qno 1 (a)
+    // int inputnumber; // declaring a variable
+
+    // printf("Enter a number : "); // asking for input 
+
+    // scanf("%d",&inputnumber); // taking input
+
+    // if (inputnumber%2==0)/*checking if input number is odd or even*/{ 
+    //     printf("%d is a prime number",inputnumber);
+    // } else {
+    //     printf("%d is odd number",inputnumber);
+    //     }
+    /************************************************************************************ */
+    //Qno 1 (b)
+    // int number5dgt, sum =0;
+    // printf("Enter a 5digit number: ");
+    // scanf("%d",&number5dgt);//input
+    // for(int i=0;i<5;i++){//running loop for 5 times 
+    //     sum+=number5dgt%10;//due to %10 we will get [-1] digit and it will store in sum variable
+    //     number5dgt/=10;// here we delete the same digit we get above
+    // }
+    // printf("SUM of a 5 digit number = %d",sum);
+    /************************************************************************************ */
+    //Qno 1 (c)
+    // for(int i = 1;i<=5;i++){
+    //     for(int j = 1; j <= i;j++){
+    //         printf(" %d ",j);
+    //     }
+    //     printf("\n");
+    // }
+    /*********************************************************************************** */
+    //Qno 3 (b)
+    // int marks;
+    // printf("Enter student marks :");
+    // scanf("%d",&marks);
+
+    // if (marks>=75 && marks <= 100){
+    //     printf("Student got A");
+    // } else if (marks >=60 && marks<75){
+    //     printf("Student got B");
+    // } else if (marks>=50 && marks<60){
+    //     printf("Student got C");
+    // } else if (marks>=40 && marks<50){
+    //     printf("Student got D");
+    // } else if (marks >= 0 && marks<40){
+    //     printf("Student got E");
+    // } else {
+    //     printf("Wrong input");
+    // }
+    
+
+    return 0;
+}
